bf810db464a0db334257af26e1f953843d6fcd79
Hidayat Wazir <mehidayatwazir@gmail.com>
2022-05-17 15:32:34 +0500

    initial

---
+++ app/src/main/java/com/example/sp20_bse_006_mid_sec_c_button_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sp20_bse_006_mid_sec_c_button_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
